aa115139a6736ac5a9d2a9b6f5496fce6deff714
Benjamin Leonard <hello@benleonard.co.uk>
2022-12-20 15:46:15 +0000

    Add figma export script

---
+++ .figmaexportrc.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+const { pascalCase } = require('@figma-export/utils')
+
+module.exports = {
+  commands: [
+    [
+      'components',
+      {
+        fileId: 'iMVpYGoNGpwMEL9gd0oeYF',
+        onlyFromPages: ['Icons'],
+        transformers: [
+          require('@figma-export/transform-svg-with-svgo')({
+            plugins: [],
+          }),
+        ],
+        outputters: [
+          require('@figma-export/output-components-as-svgr')({
+            output: './icons',
+            getFileExtension: () => '.tsx',
+            getDirname: () => '',
+            getComponentName: ({ componentName }) =>
+              pascalCase(
+                componentName
+                  .split('/')
+                  .map((n) => `${n[0].toUpperCase()}${n.slice(1)}`)
+                  .reverse()
+                  .join('') + 'Icon',
+              ),
+            getSvgrConfig: () => {
+              return {
+                jsxRuntime: 'automatic',
+                typescript: true,
+                titleProp: true,
+                svgProps: {
+                  role: 'img',
+                },
+              }
+            },
+          }),
+        ],
+      },
+    ],
+  ],
+}
