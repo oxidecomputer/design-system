b1b0f4b0e531888aff88c0dd008308d9f6bbe3a1
Justin Bennett <oss@just-be.dev>
2023-02-02 14:11:46 -0500

    Add fill replacement

--- scripts/export-icons.sh
+++ scripts/export-icons.sh
@@ -14,4 +14,7 @@ rm -rf $ICONS_DIR
 # This command requires a FIGMA_TOKEN env var with read access to Oxide's DS to be set
 npx figma-export use-config
 
+# Replace fills with current color
+sed -i 's/fill=".*"/fill="currentColor"/g' icons/*
+
 npm run fmt .
