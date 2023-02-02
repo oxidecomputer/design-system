145e92a65a8b6d9effcc6063fcc21f0f5ea168a6
Justin Bennett <oss@just-be.dev>
2023-02-02 13:57:25 -0500

    Update icon export script

--- .figmaexportrc.js
+++ .figmaexportrc.js
@@ -9,13 +9,40 @@ module.exports = {
         onlyFromPages: ['Icons'],
         transformers: [
           require('@figma-export/transform-svg-with-svgo')({
-            plugins: [],
+            plugins: [
+              {
+                name: 'preset-default',
+                params: {
+                  overrides: {
+                    removeViewBox: false,
+                  },
+                },
+              },
+              {
+                name: 'removeAttrs',
+                params: {
+                  attrs: 'svg:fill:none',
+                },
+              },
+            ],
           }),
         ],
         outputters: [
           require('@figma-export/output-components-as-svg')({
             output: './icons',
             getDirname: () => '',
+            getBasename: ({ basename, dirname }) => {
+              if (basename.includes('Direction=')) {
+                basename = `carat-${basename.split('=')[1].toLowerCase()}`
+              }
+              if (dirname === 'distro') {
+                return `distro-${basename}.svg`
+              }
+              if (!isNaN(parseInt(dirname))) {
+                return `${basename}-${dirname}.svg`
+              }
+              return `${basename}.svg`
+            },
           }),
         ],
       },
--- scripts/export-icons.sh
+++ scripts/export-icons.sh
@@ -5,7 +5,6 @@ set -o pipefail
 shopt -s nullglob
 
 ICONS_DIR="icons"
-CODEMOD_DIR="codemods"
 
 # Parse .dotenv if it exists
 export $(egrep -v '^#' .env | xargs)
@@ -14,13 +13,5 @@ rm -rf $ICONS_DIR
 
 # This command requires a FIGMA_TOKEN env var with read access to Oxide's DS to be set
 npx figma-export use-config
-
-# for file in $ICONS_DIR/*.js $ICONS_DIR/**/*.js; do
-#   mv -- "$file" "${file%.js}.ts"
-# done
-# 
-# for file in $CODEMOD_DIR/*.icons.js; do
-#     npx jscodeshift -t $file --extensions=ts,tsx --parser=tsx $ICONS_DIR
-# done
 
 npm run fmt .
