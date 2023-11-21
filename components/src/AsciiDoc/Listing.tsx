a4e1b84ad6cddfb1565990deb1ec127833daf089
Benjamin Leonard <benji@oxide.computer>
2023-11-21 16:38:54 +0000

    TLA language support (#62)
    * TLA language support
    
    * Build

--- components/src/AsciiDoc/Listing.tsx
+++ components/src/AsciiDoc/Listing.tsx
@@ -15,6 +15,28 @@ import cn from 'classnames'
 import hljs from 'highlight.js'
 import { decode } from 'html-entities'
 
+// Custom highlight.js language definition to support TLA+
+// Reference: https://github.com/highlightjs/highlight.js/pull/1658
+hljs.registerLanguage('tla', function (hljs) {
+  return {
+    keywords: {
+      keyword:
+        'ASSUME ASSUMPTION AXIOM BOOLEAN CASE CONSTANT CONSTANTS ELSE EXCEPT EXTENDS FALSE ' +
+        'IF IN INSTANCE LET LOCAL MODULE OTHER STRING THEN THEOREM LEMMA PROPOSITION COROLLARY ' +
+        'TRUE VARIABLE VARIABLES WITH CHOOSE ENABLED UNCHANGED SUBSET UNION DOMAIN BY OBVIOUS ' +
+        'HAVE QED TAKE DEF HIDE RECURSIVE USE DEFINE PROOF WITNESS PICK DEFS PROVE SUFFICES ' +
+        'NEW LAMBDA STATE ACTION TEMPORAL ONLY OMITTED ',
+    },
+    contains: [
+      hljs.QUOTE_STRING_MODE,
+      hljs.COMMENT('\\(\\*', '\\*\\)'),
+      hljs.COMMENT('\\\\\\*', '$'),
+      hljs.C_NUMBER_MODE,
+      { begin: /\/\\/ }, // relevance booster
+    ],
+  }
+})
+
 const Listing = ({ node }: { node: AdocTypes.Block }) => {
   const document = node.getDocument()
   const attrs = node.getAttributes()
