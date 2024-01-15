68fb35dd0e1c557212866eea5515db391b4a3987
Benjamin Leonard <benji@oxide.computer>
2024-01-15 15:57:39 +0000

    Update icons (#65)
    * Fmt
    
    * Update icons

--- components/src/asciidoc/Section.tsx
+++ components/src/asciidoc/Section.tsx
@@ -5,6 +5,7 @@
  *
  * Copyright Oxide Computer Company
  */
+
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -12,7 +13,6 @@
  *
  * Copyright Oxide Computer Company
  */
-
 import { Link16Icon } from '@/icons/react'
 import { type AdocTypes, Content, getRole } from '@oxide/react-asciidoc'
 import cn from 'classnames'
