a5ff0eaaf5fa85fcf69b36d6f23728c200f2e9b7
Benjamin Leonard <hello@benleonard.co.uk>
2024-05-21 19:00:22 +0100

    Update section to new format

--- components/src/asciidoc/Section.tsx
+++ components/src/asciidoc/Section.tsx
@@ -5,57 +5,29 @@
  *
  * Copyright Oxide Computer Company
  */
-
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, you can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * Copyright Oxide Computer Company
- */
 import { Link16Icon } from '@/icons/react'
-import { type AdocTypes, Content, getRole } from '@oxide/react-asciidoc'
+import { Content, type SectionBlock } from '@oxide/react-asciidoc'
 import cn from 'classnames'
 import parse from 'html-react-parser'
 import { createElement } from 'react'
 
-const Section = ({ node }: { node: AdocTypes.Section }) => {
-  const docAttrs = node.getDocument().getAttributes()
-  const level = node.getLevel()
+// We need to remove anchors from the section title (and table of contents) because having
+// an anchor within an anchor causes a client/server render mismatch
+export const stripAnchors = (str: string) => str.replace(/<a[^>]*>(.*?)<\/a>/gi, '$1')
+
+const Section = ({ node }: { node: SectionBlock }) => {
+  const level = node.level
   let title: JSX.Element | string = ''
 
-  let sectNum = node.getSectionNumeral()
+  let sectNum = node.num
   sectNum = sectNum === '.' ? '' : sectNum
-
-  const sectNumLevels = docAttrs['sectnumlevels'] ? parseInt(docAttrs['sectnumlevels']) : 3
-
-  if (node.getCaption()) {
-    title = node.getCaptionedTitle()
-  } else if (node.isNumbered() && level <= sectNumLevels) {
-    if (level < 2 && node.getDocument().getDoctype() === 'book') {
-      const sectionName = node.getSectionName()
-      if (sectionName === 'chapter') {
-        const signifier = docAttrs['chapter-signifier']
-        title = `${signifier || ''} ${sectNum} ${node.getTitle()}`
-      } else if (sectionName === 'part') {
-        const signifier = docAttrs['part-signifier']
-        title = `${signifier || ''} ${sectNum} ${node.getTitle()}`
-      } else {
-        title = node.getTitle() || ''
-      }
-    } else {
-      title = node.getTitle() || ''
-    }
-  } else {
-    title = node.getTitle() || ''
-  }
 
   title = (
     <>
-      <a className="anchor" id={node.getId() || ''} aria-hidden />
-      <a className="link group" href={`#${node.getId()}`}>
-        {parse(title)}
-        <Link16Icon className="text-accent-secondary hidden group-hover:inline-block ml-2" />
+      <span className="anchor" id={node.id || ''} aria-hidden="true" />
+      <a className="link group" href={`#${node.id}`}>
+        {parse(stripAnchors(node.title))}
+        <Link16Icon className="ml-2 hidden text-accent-secondary group-hover:inline-block" />
       </a>
     </>
   )
@@ -63,18 +35,18 @@ const Section = ({ node }: { node: AdocTypes.Section }) => {
   if (level === 0) {
     return (
       <>
-        <h1 className={cn('sect0', getRole(node))} data-sectnum={sectNum}>
+        <h1 className={cn('sect0', node.role)} data-sectnum={sectNum}>
           {title}
         </h1>
-        <Content blocks={node.getBlocks()} />
+        <Content blocks={node.blocks} />
       </>
     )
   } else {
     return (
-      <div className={cn(`sect${level}`, getRole(node))}>
+      <div className={cn(`sect${level}`, node.role)}>
         {createElement(`h${level + 1}`, { 'data-sectnum': sectNum }, title)}
         <div className="sectionbody">
-          <Content blocks={node.getBlocks()} />
+          <Content blocks={node.blocks} />
         </div>
       </div>
     )
