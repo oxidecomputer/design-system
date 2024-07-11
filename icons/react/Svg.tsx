5137195f68e5c84afceea28776786a1abe69555a
Charlie Park <charlie@oxidecomputer.com>
2024-07-11 13:25:03 -0400

    better logic around role — img or presentation

--- icons/react/Svg.tsx
+++ icons/react/Svg.tsx
@@ -24,7 +24,7 @@ export const Svg = ({
     height={size}
     viewBox={`0 0 ${size} ${size}`}
     xmlns="http://www.w3.org/2000/svg"
-    role="img"
+    role={title ? "img" : "presentation"}
     {...props}
   >
     {title && <title>{title}</title>}
