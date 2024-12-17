95a3aa165b682bb13989783fa6a445bb4ca39323
Benjamin Leonard <hello@benleonard.co.uk>
2024-12-17 11:09:15 +0000

    `useDelegatedReactRouterLinks`

---
+++ components/src/asciidoc/use-delegated-links.ts
@@ -0,0 +1,42 @@
+import { useNavigate } from '@remix-run/react'
+import { useEffect } from 'react'
+
+// Grabbed from https://github.com/remix-run/react-router-website/blob/main/app/ui/delegate-markdown-links.ts
+// Converts regular AsciiDoc a tags and makes them React Routery
+// Added key so that this is reloaded when the user routes from one document to another directly
+function useDelegatedReactRouterLinks(nodeRef: React.RefObject<HTMLElement>, key: string) {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const node = nodeRef.current
+    const handler = (event: MouseEvent) => {
+      if (!nodeRef.current) return
+
+      if (!(event.target instanceof HTMLElement)) return
+
+      let a = event.target.closest('a')
+
+      if (
+        a && // is anchor or has anchor parent
+        a.hasAttribute('href') && // has an href
+        a.host === window.location.host && // is internal
+        event.button === 0 && // left click
+        (!a.target || a.target === '_self') && // Let browser handle "target=_blank" etc.
+        !(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey) // not modified
+      ) {
+        event.preventDefault()
+        const { pathname, search, hash } = a
+        navigate({ pathname, search, hash })
+      }
+    }
+
+    if (!node) return
+    node.addEventListener('click', handler)
+
+    return () => {
+      node?.removeEventListener('click', handler)
+    }
+  }, [navigate, nodeRef, key])
+}
+
+export { useDelegatedReactRouterLinks }
