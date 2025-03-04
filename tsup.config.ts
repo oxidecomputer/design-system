185cda2238327b0bb5163bd6a91da902b17dee2c
David Crespo <david-crespo@users.noreply.github.com>
2025-03-04 14:18:02 -0600

    Actually build the icons, move output (#126)
    * actually build the icons
    
    * include svgs in package

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,9 +9,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   publicDir: 'components/src/assets/',
-  entry: ['components/src/index.ts'],
-  outDir: 'components/dist',
-  external: ['react/jsx-runtime'],
+  entry: ['components/src/index.ts', 'icons/index.ts', 'icons/react/index.ts'],
   sourcemap: true,
   clean: true,
   format: ['esm'],
