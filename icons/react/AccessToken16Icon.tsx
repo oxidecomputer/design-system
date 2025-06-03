2e8b315611528deac464cacf435c2cea2a974a89
Benjamin Leonard <benji@oxide.computer>
2025-06-03 18:59:57 +0100

    Add access token icon (and others)

---
+++ icons/react/AccessToken16Icon.tsx
@@ -0,0 +1,35 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright Oxide Computer Company
+ */
+import type { SVGProps } from 'react'
+
+interface SVGRProps {
+  title?: string
+  titleId?: string
+}
+const AccessToken16Icon = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    viewBox="0 0 16 16"
+    role="img"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      fill="currentColor"
+      d="M1.25 11a.75.75 0 0 1 .75.75V14h2.25a.75.75 0 0 1 .75.75v.5a.75.75 0 0 1-.75.75H.75a.75.75 0 0 1-.75-.75v-3.5A.75.75 0 0 1 .75 11zm14 0a.75.75 0 0 1 .75.75v3.5l-.004.077a.75.75 0 0 1-.746.673h-3.5a.75.75 0 0 1-.75-.75v-.5a.75.75 0 0 1 .75-.75H14v-2.25a.75.75 0 0 1 .75-.75zm-7-8a.75.75 0 0 1 .75.75v2.518l2.18-1.26a.75.75 0 0 1 1.025.275l.25.434a.75.75 0 0 1-.274 1.024L10 8l2.18 1.26a.75.75 0 0 1 .275 1.024l-.25.434a.75.75 0 0 1-1.024.274L9 9.731v2.519a.75.75 0 0 1-.75.75h-.5a.75.75 0 0 1-.75-.75V9.731l-2.18 1.26a.75.75 0 0 1-1.025-.274l-.25-.434a.75.75 0 0 1 .274-1.024L6 7.999l-2.18-1.258a.75.75 0 0 1-.274-1.024l.25-.434a.75.75 0 0 1 1.024-.274L7 6.268V3.75A.75.75 0 0 1 7.75 3zm-4-3A.75.75 0 0 1 5 .75v.5a.75.75 0 0 1-.75.75H2v2.25a.75.75 0 0 1-.75.75h-.5A.75.75 0 0 1 0 4.25V.75A.75.75 0 0 1 .75 0zm11.077.004A.75.75 0 0 1 16 .75v3.5a.75.75 0 0 1-.75.75h-.5a.75.75 0 0 1-.75-.75V2h-2.25a.75.75 0 0 1-.75-.75v-.5a.75.75 0 0 1 .75-.75h3.5z"
+    />
+  </svg>
+)
+export default AccessToken16Icon
