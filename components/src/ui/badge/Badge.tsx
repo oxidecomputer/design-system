be77366cffbbd4f1686b4ea91a9d8aba03deb8bb
Benjamin Leonard <benji@oxide.computer>
2025-08-15 17:32:49 +0100

    Tailwind 4 upgrade (#133)
    * Tailwind 4 upgrade test
    
    * Utility tweaks
    
    * Fix unknown `inline-code`
    
    * Add some shared tailwind breakpoints and overrides
    
    * Update styles/src/tailwind.css license
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Arbitrary var syntax fixes
    
    * Fix alpha tokens
    
    * Disable default colours
    
    * Move culori to dev deps
    
    * Tweak color var syntax
    
    * More var tweaks for better syntax
    
    * Fallback for semantic tokens with direct values
    
    * Implement prefix map suggestion
    
    * Move `inline-code` to components
    
    * Scrim / alpha var fix
    
    * Revert "Move `inline-code` to components"
    
    This reverts commit e2e4f77871b542866fc2a146ae128861101c10e9.
    
    * Fix var and floating points
    
    * Fix opacity for themed tokens with alpha
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/src/ui/badge/Badge.tsx
+++ components/src/ui/badge/Badge.tsx
@@ -25,19 +25,19 @@ export interface BadgeProps {
 
 export const badgeColors: Record<BadgeVariant, Record<BadgeColor, string>> = {
   default: {
-    default: `ring-1 ring-inset bg-accent-secondary text-accent ring-[rgba(var(--base-green-800-rgb),0.15)]`,
-    destructive: `ring-1 ring-inset bg-destructive-secondary text-destructive ring-[rgba(var(--base-red-800-rgb),0.15)]`,
-    notice: `ring-1 ring-inset bg-notice-secondary text-notice ring-[rgba(var(--base-yellow-800-rgb),0.15)]`,
-    neutral: `ring-1 ring-inset bg-secondary text-secondary ring-[rgba(var(--base-neutral-700-rgb),0.15)]`,
-    purple: `ring-1 ring-inset bg-[var(--base-purple-200)] text-[var(--base-purple-700)] ring-[rgba(var(--base-purple-800-rgb),0.15)]`,
-    blue: `ring-1 ring-inset bg-info-secondary text-info ring-[rgba(var(--base-blue-800-rgb),0.15)]`,
+    default: 'bg-accent-secondary text-accent ring-accent/15',
+    destructive: 'bg-destructive-secondary text-destructive ring-destructive/15',
+    notice: 'bg-notice-secondary text-notice ring-notice/15',
+    blue: 'bg-blue-200 text-blue-800 ring-blue-800/15',
+    purple: 'bg-purple-200 text-purple-800 ring-purple-800/15',
+    neutral: 'bg-secondary text-default ring-neutral-700/15',
   },
   solid: {
     default: 'bg-accent text-inverse',
     destructive: 'bg-destructive text-inverse',
     notice: 'bg-notice text-inverse',
     neutral: 'bg-inverse-tertiary text-inverse',
-    purple: 'bg-[var(--base-purple-700)] text-inverse',
+    purple: 'bg-purple-700 text-inverse',
     blue: 'bg-info text-inverse',
   },
 }
@@ -51,7 +51,7 @@ export const Badge = ({
   return (
     <span
       className={cn(
-        'ox-badge',
+        'ox-badge ring ring-inset',
         `variant-${variant}`,
         'inline-flex h-4 items-center whitespace-nowrap rounded-sm px-[3px] py-[1px] uppercase text-mono-sm',
         badgeColors[variant][color],
