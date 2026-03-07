3e41007e98cc79f5dd05f263735c97b3d8065f7b
Benjamin Leonard <benji@oxide.computer>
2026-03-07 21:37:53 +0000

    Tweak colours and badge

--- components/src/ui/badge/Badge.tsx
+++ components/src/ui/badge/Badge.tsx
@@ -33,12 +33,12 @@ export const badgeColors: Record<BadgeVariant, Record<BadgeColor, string>> = {
     neutral: 'bg-secondary text-default ring-current/15',
   },
   solid: {
-    default: 'bg-green-700 text-inverse',
-    destructive: 'bg-red-700 text-inverse',
-    notice: 'bg-yellow-700 text-inverse',
-    blue: 'bg-blue-700 text-inverse',
-    purple: 'bg-purple-700 text-inverse',
-    neutral: 'bg-neutral-700 text-inverse',
+    default: 'bg-green-800 text-green-200 light:bg-green-900 ring-current/15',
+    destructive: 'bg-red-800 text-red-200 light:bg-red-900 ring-current/15',
+    notice: 'bg-yellow-800 text-yellow-200 light:bg-yellow-900 ring-current/15',
+    blue: 'bg-blue-800 text-blue-200 light:bg-blue-900 ring-current/15',
+    purple: 'bg-purple-800 text-purple-200 light:bg-purple-900 ring-current/15',
+    neutral: 'bg-neutral-800 text-neutral-200 light:bg-neutral-900 ring-current/15',
   },
 }
 
